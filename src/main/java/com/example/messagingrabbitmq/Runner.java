eeda2a3ee51978ce061a0d17af94bb77a4261bcd
ItsKil0Byte <itskilobyte@gmail.com>
2025-06-15 19:09:18 +0500

    Task completed

--- src/main/java/com/example/messagingrabbitmq/Runner.java
+++ src/main/java/com/example/messagingrabbitmq/Runner.java
@@ -1,5 +1,6 @@
 package com.example.messagingrabbitmq;
 
+import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
@@ -19,9 +20,23 @@ public class Runner implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("Sending message...");
-		rabbitTemplate.convertAndSend(MessagingRabbitmqApplication.topicExchangeName, "foo.bar.baz", "Hello from RabbitMQ!");
-		receiver.getLatch().await(10000, TimeUnit.MILLISECONDS);
+		System.out.println("Enter a message or type 'q' to exit:");
+
+		Scanner scanner = new Scanner(System.in);
+
+		while (true) {
+			String message = scanner.nextLine();
+
+			if (message.equals("q")) {
+				System.out.println("Shutting down...");
+				break;
+			}
+
+			System.out.println("Sent <" + message + ">");
+
+			rabbitTemplate.convertAndSend(MessagingRabbitmqApplication.fanoutExchangeName, "", message);
+			receiver.getLatch().await(10000, TimeUnit.MILLISECONDS);
+		}
 	}
 
 }
